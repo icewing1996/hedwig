d68672f73020464e2f1a3a5d8487b7c105263ce4
Jimmy Wei <icewing1996@gmail.com>
2020-05-26 15:19:15 +0800

    transformer api update

--- models/bert/__main__.py
+++ models/bert/__main__.py
@@ -3,7 +3,7 @@ import time
 
 import numpy as np
 import torch
-from transformers import AdamW, BertForSequenceClassification, BertTokenizer, WarmupLinearSchedule
+from transformers import AdamW, BertForSequenceClassification, BertTokenizer, get_linear_schedule_with_warmup
 
 from common.constants import *
 from common.evaluators.bert_evaluator import BertEvaluator
@@ -118,8 +118,9 @@ if __name__ == '__main__':
 
     else:
         optimizer = AdamW(optimizer_grouped_parameters, lr=args.lr, weight_decay=0.01, correct_bias=False)
-        scheduler = WarmupLinearSchedule(optimizer, t_total=num_train_optimization_steps,
-                                         warmup_steps=args.warmup_proportion * num_train_optimization_steps)
+        scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps=WARMUP_STEPS, num_training_steps = num_train_optimization_steps,
+                                                    warmup_steps=args.warmup_proportion * num_train_optimization_steps )
+
 
     trainer = BertTrainer(model, optimizer, processor, scheduler, tokenizer, args)
 
